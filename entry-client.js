727acdce6a51c03614c7d74c56f002ca972d84e6
Tlacaelel Leon <radeon777@live.com>
2020-08-15 14:39:29 -0600

    Implementing code-splitting

--- entry-client.js
+++ entry-client.js
@@ -1,6 +1,9 @@
 import { createApp } from './app';
+import { Router } from 'express';
 
 const { app } = createApp();
 
 //assiming App.vue template root element has `id="app"`
-app.$mount('#app')
+Router.onReady(() => {
+    app.$mount('#app');
+})
--- router.js
+++ router.js
@@ -7,7 +7,8 @@ export function createRouter () {
     return new Router({
         mode: 'history',
         routes: [
-            //...
+            { path: '/', component: () => import('./components/Home.vue')},
+            { path: '/item/:id', component: () => import('./components/Item.vue')}
         ]
     })
 }
